ba0fad16a7caf261d75860485e8217e4de1dd848
stockholmsyndrom <33828679+stockholmsyndrom@users.noreply.github.com>
2017-12-24 12:18:14 +0300

    formmmm

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,23 +317,23 @@ var settings = {
 var myform = $("form#myform");
 myform.submit(function(event){
 	event.preventDefault();
-	
-		var params = myform.serializeArray().reduce(function(obj, item) {
+
+	var params = myform.serializeArray().reduce(function(obj, item) {
      obj[item.name] = item.value;
      return obj;
   }, {});
 
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
-  var template_id = "stockholm";
 
+  var template_id = "stockholm";
   myform.find("button").text("Sending...");
-  emailjs.sendForm(service_id,template_id,"myform")
-  	.then(function(){ 
-    	alert("Sent!");
-       myform.find("button").text("Send");
-    }, function(err) {
-       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+  emailjs.send(service_id,template_id,params)
+  	.then(function(){
+       alert("You`ve done it!");
+       myform.find("button").text("отправить");
+     }, function(err) {
+       alert("ERROR\r\n Response:\n " + JSON.stringify(err));
        myform.find("button").text("Send");
     });
   return false;
