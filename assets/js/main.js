633f62a129ace6abd4cc3893bd522ec7462f9f6c
stockholmsyndrom <33828679+stockholmsyndrom@users.noreply.github.com>
2017-12-24 12:14:42 +0300

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,6 +317,11 @@ var settings = {
 var myform = $("form#myform");
 myform.submit(function(event){
 	event.preventDefault();
+	
+		var params = myform.serializeArray().reduce(function(obj, item) {
+     obj[item.name] = item.value;
+     return obj;
+  }, {});
 
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
